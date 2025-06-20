f4548a6a99ed288d12ce6c3c1bfc01c0d7b8e9e1
lerenard226 <nikiemaibrahimcat@gmail.com>
2025-06-20 23:41:08 +0200

    Exercice 4;5 fait et ajout de leur éxercutables

--- exo04.c
+++ exo04.c
@@ -1,5 +1,24 @@
 #include <stdio.h>
 int main(void)
 {
+    // EXERCICE 4:
+    /*
+    Écrire un un programme en C qui  permet d'afficher si un nombre  entier saisi au  clavier est pair ou impair
+    */
+
+    //CODE
+    int nombre=0;
+    printf("Entrer un nombre : ");
+    scanf("%d",&nombre);
+    if (nombre%2==0)
+    {
+        printf("%d est un nombre paire\n",nombre);
+    }
+    else
+    {
+        printf("%d est un nombre impaire\n",nombre);
+    }
+    
+    
     return 0;
 }
--- exo05.c
+++ exo05.c
@@ -1,5 +1,37 @@
 #include <stdio.h>
 int main(void)
 {
+    // EXERCICE 5:
+    /*
+    Écrire un programme en C  qui permet d'afficher le plus grand de trois entiers saisis  au clavier. 
+    */
+
+    //CODE
+    int nombre1=0;
+    int nombre2=0;
+    int nombre3=0;
+    printf("Entrer un premier nombre : ");
+    scanf("%d",&nombre1);
+    printf("Entrer un deuxième nombre : ");
+    scanf("%d",&nombre2);
+    printf("Entrer un troixième nombre : ");
+    scanf("%d",&nombre3);
+    if (nombre1>nombre2&&nombre1>nombre3)
+    {
+        printf("\nle plus grand entre les trois nombres est %d\n",nombre1);
+    }
+    else if (nombre2>nombre1&&nombre2>nombre3)
+    {
+        printf("\nle plus grand entre les trois nombres est %d\n",nombre2);
+    }
+    else if (nombre3>nombre2&&nombre3>nombre1)
+    {
+        printf("\nle plus grand entre les trois nombres est %d\n",nombre3);
+    }
+    else
+    {
+        printf("\nTous les trois nombres sont égaux\n");
+    }
+    
     return 0;
 }
